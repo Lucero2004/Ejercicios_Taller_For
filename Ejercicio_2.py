fc97380a1e23e873c17cdc26161849a0e2faaf54
María Lucero Gómez Pico <125698535+Lucero2004@users.noreply.github.com>
2023-04-23 20:49:07 -0500

    Add files via upload

---
+++ Ejercicio_2.py
@@ -0,0 +1,18 @@
+# Ejercicio 2
+# Programa para determinar cuantos multiplos hay entre 1000 y 5000 de 7 y 9
+print("-----------------------------")
+print("-----MULTIPLOS DE 7 Y 9------")
+print("-----------------------------")
+
+
+
+for N in range(1000,5000):
+         rta=5000/7-142
+         print("Los multiplos de 7 entre 1000 y 5000 son:"+str(rta))
+         break
+for N in range (1000,5000):
+        Rta=  5000/9-111
+        print("Los multiplos de 9 entre 1000 y 50000 son:"+str(Rta))
+        break
+
+        
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Ejercicios_Taller_For
+# Ejercicio_2
+
+# Analisis
+mostrar e imprimir cuantos multiplos de 7 y 9 hay entre 1000 y 5000
+
+# Diseño
+
+#Diagrama de flujo
+![Diagrama de flujo](diagrama.png"Diagrama de flujo")
